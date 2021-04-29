1b0ba1afcef807ecb6e1204bd4cd1695fa6741d4
Sean Moulton <bananamoulton@gmail.com>
2021-04-29 17:14:58 -0500

    ctex tool re-write

--- ctex_tools/src/main.rs
+++ ctex_tools/src/main.rs
@@ -1,16 +1,27 @@
-#![feature(seek_stream_len)]
-
-use std::io::{Write};
-
-use ctex::par_tools::par_encode_all;
+use ctex::util::write_ctex;
+use ctex::flags::Flags;
+use glob::glob;
 
 fn main() {
-    let mut encodes = par_encode_all("input/*.png").unwrap();
+    let paths = glob("input/*.png").unwrap();
 
-    encodes.iter_mut().for_each(|(img, name)| {
-        let file = std::fs::File::create(&*format!("output/{}.ctex", name)).unwrap();
-        let mut encoder = lz4::EncoderBuilder::new().level(9).build(file).unwrap();
-        encoder.write_all(img.as_slice()).unwrap();
-        encoder.finish().1.unwrap();
+    paths.for_each(|p| {
+        let p = p.unwrap();
+        let input_name = p.clone();
+        let input_name = input_name.file_name();
+        let mut output_name = p.clone();
+        output_name.set_extension("ctex");
+        let output_name = output_name.file_name().unwrap();
+
+        let mut input_path = String::from("input/");
+        let mut output_path = String::from("output/");
+
+        input_path.push_str(input_name.unwrap().to_str().unwrap());
+        output_path.push_str(output_name.to_str().unwrap());
+
+        let output_path = output_path.as_str();
+        let input_path = input_path.as_str();
+
+        write_ctex(input_path, output_path, Flags::default()).unwrap();
     });
 }
