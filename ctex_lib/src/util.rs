f0f91a656cd71435ff7abd1c94499de68028df73
Sean Moulton <bananamoulton@gmail.com>
2021-04-29 14:44:17 -0500

    File writing utility

---
+++ ctex_lib/src/util.rs
@@ -0,0 +1,23 @@
+use crate::CtexImage;
+use crate::encode::encode_path;
+use crate::flags::Flags;
+use std::io::Write;
+
+use anyhow::Result;
+
+pub fn write_ctex(input_path: &str, output_path: &str, flags: Flags) -> Result<()> {
+    let CtexImage {
+        flags,
+        lut,
+        offsets,
+    } = encode_path(input_path, flags);
+
+    let mut file = std::fs::File::create(output_path).unwrap();
+    unsafe {
+        file.write_all(flags.as_u64().as_ne_bytes())?;
+        file.write_all(lut.as_slice().align_to::<u8>().1)?;
+        file.write_all(offsets.as_slice())?;
+    }
+
+    Ok(())
+}
