6239698770150264e8bf8030a788074e49c867fb
Sean Moulton <bananamoulton@gmail.com>
2021-04-29 14:16:33 -0500

    Version 0.2 updates

--- ctex_lib/src/lib.rs
+++ ctex_lib/src/lib.rs
@@ -1,36 +1,18 @@
 #![feature(num_as_ne_bytes)]
+#![feature(stdsimd)]
+
+use crate::flag::Flags;
 
 pub mod decode;
 pub mod encode;
 
-use anyhow::*;
+mod flag;
+mod tests;
 
-#[cfg(feature = "par_tools")]
-pub fn par_encode_all(glob_str: &str) -> Result<Vec<(Vec<u8>, String)>> {
-    use rayon::prelude::*;
-    let mut paths = Vec::new();
-    paths.extend(glob::glob(&*glob_str)?);
+const SECTOR_SIZE: usize = 64;
 
-    paths
-        .into_par_iter()
-        .flatten()
-        .map(encode::encode)
-        .collect::<Vec<Result<(Vec<u8>, String)>>>()
-        .into_iter()
-        .collect::<Result<Vec<(Vec<u8>, String)>>>()
-}
-
-#[cfg(feature = "par_tools")]
-pub fn par_decode_all(glob_str: &str) -> Result<Vec<Vec<u8>>> {
-    use rayon::prelude::*;
-    let mut paths = Vec::new();
-    paths.extend(glob::glob(&*glob_str)?);
-
-    paths
-        .into_par_iter()
-        .flatten()
-        .map(decode::decode_path)
-        .collect::<Vec<Result<Vec<u8>>>>()
-        .into_iter()
-        .collect::<Result<Vec<Vec<u8>>>>()
+pub struct CtexImage {
+    flags: Flags,
+    lut: Vec<u32>,
+    offsets: Vec<u8>,
 }
